62df745f84f8be3bf776b71c32da972847dfbf03
tarunsaipamulapati <tarunsai.pamulapati@gmail.com>
2024-05-30 02:01:28 -0600

    New changes

--- Step_trainer_landing_to_trusted.py
+++ Step_trainer_landing_to_trusted.py
@@ -24,15 +24,11 @@ CustomerCurated_node1717042289065 = glueContext.create_dynamic_frame.from_option
 # Script generated for node Step Trainer Landing
 StepTrainerLanding_node1717044110420 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://tsp-lakehouse/step_trainer/landing/"], "recurse": True}, transformation_ctx="StepTrainerLanding_node1717044110420")
 
-# Script generated for node Join
-Join_node1717042391809 = Join.apply(frame1=CustomerCurated_node1717042289065, frame2=StepTrainerLanding_node1717044110420, keys1=["serialnumber"], keys2=["serialnumber"], transformation_ctx="Join_node1717042391809")
-
 # Script generated for node SQL Query
 SqlQuery0 = '''
-select serialNumber,sensorReadingTime,distanceFromObject  from myDataSource
-
+select cc.serialNumber,sensorReadingTime, distanceFromObject from cc join stl on cc.serialnumber=stl.serialnumber;
 '''
-SQLQuery_node1717048341916 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":Join_node1717042391809}, transformation_ctx = "SQLQuery_node1717048341916")
+SQLQuery_node1717048341916 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"cc":CustomerCurated_node1717042289065, "stl":StepTrainerLanding_node1717044110420}, transformation_ctx = "SQLQuery_node1717048341916")
 
 # Script generated for node Step Trainer Trusted
 StepTrainerTrusted_node1717042469254 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1717048341916, connection_type="s3", format="json", connection_options={"path": "s3://tsp-lakehouse/step_trainer/trusted/", "partitionKeys": []}, transformation_ctx="StepTrainerTrusted_node1717042469254")
